fee3e0af95080d8d94933fc670aa995f3592081d
Mikuru-creator <mikuru0101basket@icloud.com>
2025-05-03 23:04:53 +0900

    課題　Javascript基礎9
    イベント発火　基礎編

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
-let lists = document.getElementsByClassName("pull-down-list");
-console.log(lists)
+window.addEventListener('load',function(){
+    const pullDownButton = document.getElementById("lists");
+    pullDownButton.addEventListener("mouseover", function() {
+    console.log("乗る");
+    });
+
+    pullDownButton.addEventListener("click", function() {
+    console.log("クリック");
+    });
+
+    pullDownButton.addEventListener("mouseleave", function() {
+    console.log("外れる");
+  });
+})
+
