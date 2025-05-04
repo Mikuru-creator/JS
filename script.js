53b2c12a99c654e460c4eff1be5843bd682549d2
Mikuru-creator <mikuru0101basket@icloud.com>
2025-05-04 09:45:51 +0900

    課題　Javascript基礎10
    イベント発火　基礎編

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
 window.addEventListener('load',function(){
     const pullDownButton = document.getElementById("lists");
     pullDownButton.addEventListener("mouseover", function() {
-    console.log("乗る");
+        console.log("乗った時は青色");
     });
 
     pullDownButton.addEventListener("click", function() {
-    console.log("クリック");
+        console.log("クリックした時は緑色");
     });
 
     pullDownButton.addEventListener("mouseleave", function() {
-    console.log("外れる");
+        console.log("外れた時は赤色");
   });
+
+    pullDownButton.addEventListener("mouseover", function() {
+        pullDownButton.setAttribute("style","background-color:blue;")
+    });
+
+    pullDownButton.addEventListener("click", function() {
+        pullDownButton.setAttribute("style","background-color:green;")
+    });
+
+    pullDownButton.addEventListener("mouseout", function() {
+        pullDownButton.removeAttribute("style","background-color:red;")
+    });
 })
 
