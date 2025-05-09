59166032cfad6e6238b2c99323ba3cd912ad3029
Mikuru-creator <mikuru0101basket@icloud.com>
2025-05-09 12:43:37 +0900

    課題　Javascript基礎12
    文字列の取得編

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-window.addEventListener('load',function(){
+function pullDown() {
     const pullDownButton = document.getElementById("lists");
-    const pullDownParents = document.getElementById("pull-down")
+    const pullDownParents = document.getElementById("pull-down");
+    const pullDownChild = document.querySelectorAll(".pull-down-list");
+    const currentList = document.getElementById("current-list");
 
     pullDownButton.addEventListener("mouseover", function() {
-        this.setAttribute("style","background-color:blue;")
+        this.setAttribute("style","background-color:blue;");
         console.log("乗った時は青色");
     });
 
@@ -20,5 +22,19 @@ window.addEventListener('load',function(){
     pullDownButton.addEventListener("mouseleave", function() {
         console.log("外れた時は赤色");
         this.removeAttribute("style","background-color:red;");
-  });
-})
+    });
+
+    pullDownChild.forEach(function(list, index) {
+        list.addEventListener('click', function() {
+            const value = list.innerHTML;
+            currentList.innerHTML = value;
+            console.log(value);
+
+            if (index === 0) {
+                location.href = "https://fp-curriculum.com/";
+            }
+        });
+    });
+}
+
+window.addEventListener('load',pullDown)
